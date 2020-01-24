fffd9fb2ba07ad968b7721e380c6c17f8e6a06d6
XrosLynx <guipemichaeljohn@gmail.com>
2020-01-24 11:12:23 +0800

    updated the ui

--- src/app/shared/student-thumbnail.component.ts
+++ src/app/shared/student-thumbnail.component.ts
@@ -1,17 +1,23 @@
 import {Component, Input} from '@angular/core'
 import { IStudent } from '../shared/index'
-
 @Component({
   selector: 'student-thumbnail',
   template: `
-  <div [routerLink]="['/students',student.id]" class="list-group-item">
-    <h2 >{{student?.name | uppercase}}</h2>
-      <div>Age: {{student?.age}}</div>
+  <div [routerLink]="['/students',student.id]">
+    <h2 class="list-group-item active">{{student?.name | uppercase}}</h2>
+      <div class="list-group-item">
+        <div>Age: {{student?.age}}</div>
       <div>Address: {{student?.address}}</div>
       <div *ngIf="student?.gwa">GWA: {{student?.gwa}}</div>
       <div *ngIf="student?.grade">Grade: {{student?.grade}}</div>
+      </div>
   </div>
-  `
+  `,
+  styles:[`
+  .list-group-item{
+    margin: 0;
+  }
+  `]
 })
 
 export class StudentThumbnailComponent{
--- src/app/student-list/student-list.component.ts
+++ src/app/student-list/student-list.component.ts
@@ -7,9 +7,15 @@ import { IStudent } from '../shared/index';
 @Component({
   template: `
   <div class="row">
+  <div class="col-md-12 py-4 text-left">
+    <h1>
+      List of Students
+    </h1>
+    <hr/>
+  </div>
     <div *ngFor = "let student of student" class="col-md-5">
     <student-thumbnail [student]= "student"></student-thumbnail>
-    </div>
+  </div>
   </div>
   `,
 })
