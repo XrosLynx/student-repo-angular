7e6d44343eb32608531c825886dc1488343ba2fb
XrosLynx <guipemichaeljohn@gmail.com>
2020-01-24 17:41:48 +0800

    some minor changes

--- src/app/student-details/student-details.component.ts
+++ src/app/student-details/student-details.component.ts
@@ -3,6 +3,7 @@ import {StudentService} from '../service/student.service';
 import {ActivatedRoute} from '@angular/router';
 import { IStudent } from '../shared/index';
 import { ToastrService }  from '../common/toastr-service';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   templateUrl: './student-details.component.html',
@@ -13,6 +14,7 @@ import { ToastrService }  from '../common/toastr-service';
 
 export class StudentDetailsComponent implements OnInit{
   student: IStudent
+  studentDetails: FormGroup
 
   constructor(private studentService: StudentService, private route: ActivatedRoute, private toastr: ToastrService){
 
@@ -20,6 +22,20 @@ export class StudentDetailsComponent implements OnInit{
   
   ngOnInit(){
     this.student = this.studentService.getStudent(+this.route.snapshot.params['id'])
+
+    let name = new FormControl(this.student.name)
+    let age = new FormControl(this.student.age)
+    let addr = new FormControl(this.student.address)
+    let gwa = new FormControl(this.student.gwa)
+
+    this.studentDetails = new FormGroup({
+      name: name,
+      age: age,
+      addr: addr,
+      gwa: gwa
+    })
+
+    
   }
 
    handleToastr(dataName){
