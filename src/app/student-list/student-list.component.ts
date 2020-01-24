b8a9733bc2b410c6e6c99409b8543de15a6d0cbf
XrosLynx <guipemichaeljohn@gmail.com>
2020-01-24 10:19:45 +0800

    some minor  changes

--- src/app/student-list/student-list.component.ts
+++ src/app/student-list/student-list.component.ts
@@ -7,7 +7,7 @@ import { IStudent } from '../shared/index';
 @Component({
   template: `
   <div class="row">
-    <div ngFor = "let student of student" class="col-md-5">
+    <div *ngFor = "let student of student" class="col-md-5">
     <student-thumbnail [student]= "student"></student-thumbnail>
     </div>
   </div>
